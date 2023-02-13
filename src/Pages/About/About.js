1ee5195a35193626e75b7e554e773c887d505de3
mb-tousif <meta.block.tousif@gmail.com>
2023-02-14 00:03:43 +0600

    Loader added

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import LoadingSpinner from '../../Components/LoadingSpinner'
 
 export default function About() {
   return (
     <div>
         <h1 className='text-rose-600 text-2xl text-center'>About Page</h1>
+        <LoadingSpinner/>
     </div>
   )
 }
