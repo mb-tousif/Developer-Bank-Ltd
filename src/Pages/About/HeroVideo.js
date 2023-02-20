6cd9287c9f7c4ea07b96df4613d4521b8b04840f
mb-tousif <meta.block.tousif@gmail.com>
2023-02-20 11:48:02 +0600

    Video added

--- src/Pages/About/HeroVideo.js
+++ src/Pages/About/HeroVideo.js
@@ -5,7 +5,7 @@ export default function HeroVideo() {
   return (
     <div className="">
     <div className="p-4">
-        <video className="h-[300px]" src={Video} width="100%" controls/>
+      <video className="w-full h-60" src={Video} width="100%" controls />
     </div>
     <div className=""></div>
 </div>
