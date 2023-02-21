982c28f63834f1b72e0812adc5dea70af4271c58
arifa-akter <arifaakterchoity@gmail.com>
2023-02-21 20:00:31 +0600

    home page navber

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <div className="h-36 bg-[#344c4d] mx-auto py-5 text-gray-50">
+    <div className="h-36 bg-[#142026] mx-auto py-5 text-gray-50">
       <h1 className="text-2xl text-center">𝕯𝖊𝖛𝖊𝖑𝖔𝖕𝖊𝖗 𝕭𝖆𝖓𝖐 𝕷𝖙𝖉.</h1>
       <p className="text-base text-center">
         Copyright &#169; {new Date().getFullYear()} Inc. All right reserved.
