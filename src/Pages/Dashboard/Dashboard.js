da65f1437c8125c19789bc46b5d4fb6702e1ebad
mb-tousif <meta.block.tousif@gmail.com>
2023-02-23 00:10:49 +0600

    index.css file changed

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Dashboard() {
   return (
-    <div className='text-3xl container'>
+    <div className='text-3xl container text-gray-50'>
         <h1 className='text-center'>Dashboard Home</h1>
     </div>
   )
