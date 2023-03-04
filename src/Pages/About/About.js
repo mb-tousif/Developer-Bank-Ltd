3ac3f5da95c58b5d08abb6a6acb4c56c029808f1
mb-tousif <meta.block.tousif@gmail.com>
2023-03-04 23:27:09 +0600

    --

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -5,7 +5,7 @@ import HeroVideo from './HeroVideo'
 export default function About() {
   return (
     <div className='pt-[120px]'>
-        <h1 className='text-rose-600 text-2xl text-center'>About Page</h1>
+        <h1 className='text-rose-600 text-2xl text-center'>About Page Coming Soon</h1>
         <HeroVideo/>
         <LoadingSpinner/>
     </div>
